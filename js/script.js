f50faba453a41b4803799ae06f1a20ccd51ce3c6
mixelio <78027735+mixelio@users.noreply.github.com>
2021-04-15 15:09:00 +0300

    final

---
+++ js/script.js
@@ -0,0 +1,10 @@
+$(document).ready(function () {
+  $(".slider").slick({
+    arrows: true,
+    slidesToShow: 1,
+    infinite: true,
+    speed: 500,
+    autoplay: true,
+    autoplaySpeed: 6000,
+  });
+});
