532acdd19cf1e2478bc182b604465aa86385ba97
mixelio <78027735+mixelio@users.noreply.github.com>
2021-04-22 22:06:33 +0300

    1234

--- js/script.js
+++ js/script.js
@@ -1,10 +1,12 @@
+"use strict";
+
 $(document).ready(function () {
   $(".slider").slick({
     arrows: true,
     slidesToShow: 1,
     infinite: true,
     speed: 500,
-    //  autoplay: true,
+    autoplay: true,
     autoplaySpeed: 6000,
   });
 });
