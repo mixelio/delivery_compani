95061ec636460d6ccc08ad4daa681a6d840e3c80
mixelio <78027735+mixelio@users.noreply.github.com>
2021-04-23 20:00:03 +0300

    111

--- js/script.js
+++ js/script.js
@@ -6,7 +6,40 @@ $(document).ready(function () {
     slidesToShow: 1,
     infinite: true,
     speed: 500,
-    autoplay: true,
+    //autoplay: true,
     autoplaySpeed: 6000,
   });
 });
+
+const isMobile = {
+  Android: function () {
+    return navigator.userAgent.match(/Android/i);
+  },
+  BlackBerry: function () {
+    return navigator.userAgent.match(/BlackBerry/i);
+  },
+  iOS: function () {
+    return navigator.userAgent.match(/iPhone|iPad|iPod/i);
+  },
+  Opera: function () {
+    return navigator.userAgent.match(/Opera Mini/i);
+  },
+  Windows: function () {
+    return navigator.userAgent.match(/IEMobile/i);
+  },
+  any: function () {
+    return (
+      isMobile.Android() ||
+      isMobile.BlackBerry() ||
+      isMobile.iOS() ||
+      isMobile.Opera() ||
+      isMobile.Windows()
+    );
+  },
+};
+
+if (isMobile.any()) {
+  document.body.classList.add("_touch");
+} else {
+  document.body.classList.add("_pc");
+}
