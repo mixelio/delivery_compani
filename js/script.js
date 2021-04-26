82cbc50d03095d3c791c2929e81e6aaf56ecc6cd
mixelio <78027735+mixelio@users.noreply.github.com>
2021-04-26 10:50:30 +0300

    подменю

--- js/script.js
+++ js/script.js
@@ -40,6 +40,16 @@ const isMobile = {
 
 if (isMobile.any()) {
   document.body.classList.add("_touch");
+
+  let menuArrows = document.querySelectorAll(".menu__arrow");
+  if (menuArrows.length > 0) {
+    for (let index = 0; index < menuArrows.length; index++) {
+      const menuArrow = menuArrows[index];
+      menuArrow.addEventListener("click", function (e) {
+        menuArrow.parentElement.classList.toggle("_active");
+      });
+    }
+  }
 } else {
   document.body.classList.add("_pc");
 }
